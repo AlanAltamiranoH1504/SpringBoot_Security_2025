6047f481e0bc991b7cfd2dc55d0981519b6577ad
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-01 18:44:34 -0600

    feat: add spring security configuration class
    - add password hashed method
    
    - add authenticationManager method
    
    - add SecurityFilterChain method

---
+++ src/main/java/altamirano/hernandez/app_springsecurity_2025/security/SpringSecurityConfif.java
@@ -0,0 +1,60 @@
+package altamirano.hernandez.app_springsecurity_2025.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+//Clase de configuracion de SpringSecurity
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfif {
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    //Metodo aunthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //Hasheo de Password
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Metodo seucurityFilterChain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        //Rutas que no requieren proteccion
+                        .requestMatchers(HttpMethod.GET, "/home").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/liberadas/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/acceso/registro").permitAll()
+
+                        //Rutas que requieren proteccion por roles
+                        .requestMatchers(HttpMethod.GET, "/protegido/**").hasRole("ADMIN")
+
+                        //Archivos estaticos liberadoros de proteccion
+                        .requestMatchers("/css/**", "/js/**", "/imgs/**", "/static/**").permitAll()
+
+                        //Configuraciones generales
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+        return http.build();
+    }
+}
